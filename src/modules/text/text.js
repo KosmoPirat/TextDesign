c1fd155074418852b49e2cce44a89f40a81777fb
KosmoPirat <kosmopirat@gmail.com>
2019-04-25 16:53:07 +0300

    Update text.js

--- src/modules/text/text.js
+++ src/modules/text/text.js
@@ -5,9 +5,7 @@ import {markdown} from 'markdown';
 let input = document.getElementById("text-input");
 let output = document.getElementById("preview");
 input.addEventListener("input", function () {
-    console.log(input.value + "\n");
     output.innerHTML = markdown.toHTML(input.value);
-    console.log(markdown.toHTML(input.value));
 });
 
 
