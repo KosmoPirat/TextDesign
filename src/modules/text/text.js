1a3d98577d63cfca3be11ccc197c04805bc4e317
KosmoPirat <kosmopirat@gmail.com>
2019-04-25 16:51:41 +0300

    dam redone done done

--- src/modules/text/text.js
+++ src/modules/text/text.js
@@ -5,7 +5,9 @@ import {markdown} from 'markdown';
 let input = document.getElementById("text-input");
 let output = document.getElementById("preview");
 input.addEventListener("input", function () {
+    console.log(input.value + "\n");
     output.innerHTML = markdown.toHTML(input.value);
+    console.log(markdown.toHTML(input.value));
 });
 
 
