208f5c645c87659b56c0af11b37509aa7152a044
KosmoPirat <kosmopirat@gmail.com>
2019-04-25 14:40:33 +0300

    done markdown module

--- src/modules/text/text.js
+++ src/modules/text/text.js
@@ -1,1 +1,13 @@
 import "./text.css"
+
+import {markdown} from 'markdown';
+
+function Editor(input, preview) {
+    this.update = function () {
+        preview.innerHTML = markdown.toHTML(input.value);
+    };
+    input.editor = this;
+    this.update();
+}
+let $ = function (id) { return document.getElementById(id); };
+new Editor($("text-input"), $("preview"));
