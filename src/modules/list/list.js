66cd12b564bb8caa4e4de7dd93c6ebf510cbb7f8
KosmoPirat <kosmopirat@gmail.com>
2019-04-30 15:07:43 +0300

    Update list.js

--- src/modules/list/list.js
+++ src/modules/list/list.js
@@ -27,7 +27,7 @@ let listItem = {
         this.listHead = this.getListHead();
         this.listText = this.getListText();
         console.log(innerText);
-        innerText.split('\n');
+        innerText.split(/&#13;&#10;/);
         console.log(innerText[0]);
         console.log(innerText[1]);
         this.listHead.innerHTML =  innerText[0] || "Новая заметка";
