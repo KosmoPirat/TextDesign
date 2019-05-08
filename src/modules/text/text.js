84b3d80fd2f8c98a1b88e18086cca545c2d2a11a
KosmoPirat <kosmopirat@gmail.com>
2019-05-08 10:19:19 +0300

    1

--- src/modules/text/text.js
+++ src/modules/text/text.js
@@ -1,11 +1,11 @@
-import "./text.css";
-import "../list/list.js";
+import './text.css';
+import '../list/list.js';
 import {markdown} from 'markdown';
 
-let input = document.getElementById("text-input");
-let output = document.getElementById("preview");
-input.addEventListener("input", function () {
-    output.innerHTML = markdown.toHTML(input.value);
+let input = document.getElementById('text-input');
+let output = document.getElementById('preview');
+input.addEventListener('input', function () {
+	output.innerHTML = markdown.toHTML(input.value);
 });
 
 
